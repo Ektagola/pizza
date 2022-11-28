acb8b44cb738e636184cdb348913a953314c072f
Ektagola <ektag8097@gmail.com>
2022-11-28 20:15:14 +0530

    website ready

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,45 @@
 /**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
-
-/**
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  plugins: [],
-}
+  siteMetadata : {
+    siteUrl: 'https://www.pizzapizza.com',
+    title: "Order Pizza Online || Pizza Pizza",
+    description: "Order your favourites from Pizza Pizza & get 15% Off upto Rs.300 on minimum order of Rs.600. Experience Safe, Hygienic & Contactless Delivery.Trust in Every Bite. 30 Minutes Delivery. Cashless Payment Option. Delivery In 30 Minutes.",
+    image: ''
+},
+  plugins: [
+    `@chakra-ui/gatsby-plugin`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `image`,
+        path: `${__dirname}/src/assets/images/`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-web-font-loader",
+      options: {
+        google: {
+          families: ["Droid Sans", "Droid Serif", "Abril Fatface"],
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `PizzaPizza`,
+        short_name: `Pizza`,
+        start_url: `/`,
+        background_color: `white`,
+        theme_color: `#ff500f`,
+        display: `standalone`,
+        icon: `src/assets/short_logo.png`
+      },
+    },
+  ],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,23 @@
 import * as React from "react"
+import Layout from "../components/layout"
+import Seo from '../components/seo'
+import Banner from "../sections/banner"
+import CallToAction from "../sections/callToAction"
+import Menu from "../sections/menu"
+import Service from "../sections/service"
+import SpecialOffer from "../sections/specialOffer"
+import Testimonials from "../sections/testimonial"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <Seo/>  
+      <Banner/>
+      <Service/>
+      <Menu/>
+      <SpecialOffer/>
+      <Testimonials/>
+      <CallToAction/>
+    </Layout>
+  )
 }
